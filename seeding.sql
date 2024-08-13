0ddf281baa70dd36f8e3027dc5ab055f5ae927da
Tatiana Zambrano Callejas <TatianaZambranoCallejas@gmail.com>
2024-08-13 16:06:00 +0200

    Challenge 3 - Seeding the Database

--- seeding.sql
+++ seeding.sql
@@ -4,6 +4,7 @@ VALUES (1, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
        (2, 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
        (3, 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
        (4, 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
+       (5, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray'),
        (6, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
 
 INSERT INTO `lab_db_creation`.`Customers` (`ID`, `customerID`, `name`, `phone_number`, `email`, `address`, `city`, `state/province`, `country`, `zip/postal code`) 
